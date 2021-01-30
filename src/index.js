54fd650524c39e6e2010a4780cc646662b8112c9
Bhaavani <vani.uideveloper@gmail.com>
2021-01-29 22:46:23 -0500

    Creating new index file with basic component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// mport the Rect and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// Create react component
+const App = () => {
+  return <div>Hi There!!</div>;
+};
+// Take the react component and display on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
