d599ffc2fb6326e7b72969b049bfb2169ebce37a
Bhaavani <vani.uideveloper@gmail.com>
2021-02-01 16:44:32 -0500

    HTML vs JSX until twentyOne

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 // mport the Rect and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+
+function getButtonText() {
+  return "Click on Me!";
+}
 // Create react component
 const App = () => {
-  return <div>Hi There!!</div>;
+  const buttonText = "Click Me!";
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText} //using variable
+        {getButtonText()} //using function
+      </button>
+    </div>
+  );
 };
 // Take the react component and display on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
